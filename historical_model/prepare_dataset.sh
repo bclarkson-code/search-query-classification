f2a626446cca3af8ac18fed09693b008e2abcc61
Benedict Clarkson <benedictc@spotify.com>
2021-08-14 11:09:00 +0100

    Only download datset if it does not already exist

--- historical_model/prepare_dataset.sh
+++ historical_model/prepare_dataset.sh
@@ -1,5 +1,8 @@
 export XRT_TPU_CONFIG="localservice;0;localhost:51011"
 unset LD_PRELOAD
-mkdir datasets
-gsutil -m cp gs://search-query-classification-us-central1-c/aol_data_* datasets
+if [ ! -d "datasets" ]; then
+  mkdir datasets
+  gsutil -m cp gs://search-query-classification-us-central1-c/aol_data_* datasets
+fi
+
 python3 prepare_dataset.py
