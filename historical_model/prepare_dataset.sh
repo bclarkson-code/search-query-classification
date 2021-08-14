846b5badab09a0b7d9b6374b7508cb835b05e4a6
Benedict Clarkson <benedictc@spotify.com>
2021-08-14 11:12:28 +0100

    Added download for model checkpoints

--- historical_model/prepare_dataset.sh
+++ historical_model/prepare_dataset.sh
@@ -4,5 +4,9 @@ if [ ! -d "datasets" ]; then
   mkdir datasets
   gsutil -m cp gs://search-query-classification-us-central1-c/aol_data_* datasets
 fi
+if [ ! -d "gpt2-checkpoints" ]; then
+  gsutil -m cp gs://search-query-classification-us-central1-c/gpt2-checkpoints.zip .
+  unzip gpt-checkpoints.zip
+fi
 
 python3 prepare_dataset.py
