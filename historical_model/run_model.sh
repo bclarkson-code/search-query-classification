e5ebc1377964879bb444ac3fb601bb53fc03e86f
Benedict Clarkson <benedictc@spotify.com>
2021-08-15 22:15:21 +0100

    added missing .

--- historical_model/run_model.sh
+++ historical_model/run_model.sh
@@ -1,6 +1,6 @@
 export XRT_TPU_CONFIG="localservice;0;localhost:51011"
 unset LD_PRELOAD
-gsutil cp gs://search-query-classification-us-central1-c/aol_input_data.zip
+gsutil cp gs://search-query-classification-us-central1-c/aol_input_data.zip .
 unzip -o aol_input_data.zip
 rm -f aol_input_data.zip
 python3 run_model.py
