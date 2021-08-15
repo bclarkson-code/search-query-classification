ebaecff395de33e38f0ddb29b50d0109e288f067
Benedict Clarkson <benedictc@spotify.com>
2021-08-15 23:12:10 +0100

    moved to a single gpu

--- historical_model/run_model.py
+++ historical_model/run_model.py
@@ -16,8 +16,7 @@ if __name__ == '__main__':
     )
     Path('model_save').mkdir(exist_ok=True)
     trainer = pl.Trainer(
-        gpus=4,
-        accelerator='ddp',
+        gpus=1,
         max_epochs=5,
         progress_bar_refresh_rate=1,
         logger=tb_logger,
