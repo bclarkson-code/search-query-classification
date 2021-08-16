723d59111b2a4fd6edb8bfbc42971602e47c2a7f
Benedict Clarkson <benedictc@spotify.com>
2021-08-16 21:27:38 +0100

    split data into train-test-valid

--- pretrain_model/dataset.py
+++ pretrain_model/dataset.py
@@ -1,32 +1,30 @@
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 import os
-import subprocess
-import shlex
-from pathlib import Path
 from transformers import RobertaTokenizerFast, LineByLineTextDataset, DataCollatorForLanguageModeling
+from tokenizers.pre_tokenizers import Whitespace
+from tokenizers import ByteLevelBPETokenizer
 
 
 class SearchQueryPreTrainingDataModule(pl.LightningDataModule):
     def __init__(
             self,
-            data_path: str = "open_source_text_datasets",
-            tokeniser_path: str = 'open_source_tokeniser',
+            data_path: str = "raw_queries.txt",
+            tokeniser_path: str = "tokeniser",
             batch_size: int = 128,
             num_workers: int = None,
             max_length: int = 24,
-            gs_bucket_path: str = 'gs://search-query-classification-us-central1-c/',
             mlm_probability: float = 0.15,
             debug=False
+
     ):
 
         super().__init__()
         self.data_path = data_path
+        self.tokeniser_path = tokeniser_path
         self.batch_size = batch_size
         self.max_length = max_length
-        self.gs_bucket_path = gs_bucket_path
         self.mlm_probability = mlm_probability
-        self.tokeniser_path = tokeniser_path
         self.debug = debug
         if num_workers is None:
             self.num_workers = os.cpu_count()
@@ -38,64 +36,32 @@ class SearchQueryPreTrainingDataModule(pl.LightningDataModule):
         self.valid = None
         self.data_collator = None
 
-    def _download_file(self, file_path, print_output=False):
-        """
-        Download a file from gcs
-        :param file_path: the path of the file in the gcs bucket
-        """
-        zip_path = file_path + '.zip'
-        gsutil_path = os.path.join(self.gs_bucket_path, zip_path)
-        download_cmd = shlex.split(
-            f'gsutil cp {gsutil_path} .'
-        )
-        unzip_cmd = shlex.split(
-            f'unzip -o {zip_path}'
-        )
-        clean_up_cmd = shlex.split(
-            f'rm -f {zip_path}'
-        )
-        commands = [download_cmd, unzip_cmd, clean_up_cmd]
-        for cmd in commands:
-            if print_output:
-                print(subprocess.check_output(
-                    cmd,
-                    stderr=subprocess.STDOUT).decode('utf-8')
-                )
-            else:
-                subprocess.call(cmd)
-
-    def _download_tokeniser(self):
-        print('Downloading Tokeniser...', end='')
-        self._download_file(self.tokeniser_path)
-        assert os.path.exists(self.tokeniser_path)
-        print('Done')
-
-    def _download_data(self):
-        print('Downloading Data...', end='')
-        self._download_file(self.data_path, print_output=self.debug)
-        assert os.path.exists(self.data_path)
-        print('Done')
-        # if debug, only take the first 500 lines of each text file
-        if self.debug:
-            print()
-            for ds_name in ['train', 'test', 'valid']:
-                ds_file = os.path.join(self.data_path, f'{ds_name}.txt')
-                with open(ds_file, 'r') as f:
-                    data = [line for line in f]
-                with open(ds_file, 'w') as f:
-                    f.write('\n'.join(data[:5000]))
-
+    def train_tokeniser(self):
+        self.tokenizer = ByteLevelBPETokenizer(lowercase=True)
+        self.tokenizer.pre_tokenizer = Whitespace()
+        self.tokenizer.train(
+            files=[self.data_path],
+            vocab_size=50000,
+            min_frequency=2,
+            show_progress=True,
+            special_tokens=[
+                "<s>",
+                "<pad>",
+                "</s>",
+                "<unk>",
+                "<mask>",
+            ])
+        self.tokenizer.save_model(self.tokeniser_path)
 
     def prepare_data(self):
         # Build tokeniser
-        if not os.path.exists(self.tokeniser_path):
-            self._download_tokeniser()
-        self.tokeniser = RobertaTokenizerFast.from_pretrained(
-            self.tokeniser_path,
-            max_len=self.max_length
-        )
-        # Download raw data
-        self._download_data()
+        if os.path.exists(self.tokeniser_path):
+            self.train_tokeniser()
+        else:
+            self.tokeniser = ByteLevelBPETokenizer(
+                os.path.abspath(os.path.join(self.tokeniser_path, 'vocab.json')),
+                os.path.abspath(os.path.join(self.tokeniser_path, 'merges.txt'))
+            )
 
         # Build datasets
         for ds_name in ['train', 'test', 'valid']:
@@ -143,3 +109,7 @@ class SearchQueryPreTrainingDataModule(pl.LightningDataModule):
             shuffle=False,
             num_workers=self.num_workers
         )
+
+if __name__ == '__main__':
+    ds = SearchQueryPreTrainingDataModule()
+    ds.prepare_data()
--- pretrain_model/prepare_dataset.py
+++ pretrain_model/prepare_dataset.py
@@ -3,6 +3,8 @@ import pandas as pd
 from tqdm.auto import tqdm
 import subprocess
 import shlex
+import os
+import numpy as np
 
 if __name__ == '__main__':
     Path('raw_dataset').mkdir(exist_ok=True)
@@ -17,7 +19,22 @@ if __name__ == '__main__':
     )
 
     df = pd.read_parquet('raw_dataset')
+
     # write the strings to a text file for the pretrain model
-    with open('raw_queries.txt', 'w') as f:
-        for query in tqdm(df['query_text']):
-            f.write(query)
+    text_ds_dir = 'text_datasets'
+    Path(text_ds_dir).mkdir(exist_ok=True)
+    train_frac = 0.8
+    valid_frac = 0.1
+    n_queries = len(df)
+    datasets = np.split(
+        df.sample(frac=1, random_state=42),
+        [
+            int(train_frac * n_queries),
+            int((train_frac + valid_frac) * n_queries)
+        ]
+    )
+    for ds, ds_name in zip(datasets, ['train', 'test', 'valid']):
+        file_path = os.path.join(text_ds_dir, f'{ds_name}.txt')
+        with open(file_path, 'w') as f:
+            for query in tqdm(df['query_text'], dec=f'Writing {ds_name} queries to file'):
+                f.write(query)
