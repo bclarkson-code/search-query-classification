11dace861232b030329939e9e1bfb56c67953dc5
Benedict Clarkson <benedictc@spotify.com>
2021-08-17 17:18:00 +0100

    removed tokeniser from datacollator

--- pretrain_model/dataset.py
+++ pretrain_model/dataset.py
@@ -106,7 +106,6 @@ class SearchQueryPreTrainingDataModule(pl.LightningDataModule):
             max_len=self.max_length,
         )
         self.data_collator = DataCollatorForLanguageModeling(
-            tokenizer=self.tokeniser,
             mlm=True,
             mlm_probability=self.mlm_probability
         )
