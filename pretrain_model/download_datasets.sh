66afc9fb53a5a2585a240255b688adb62095fa4b
Benedict Clarkson <benedictc@spotify.com>
2021-08-18 13:19:15 +0100

    added text datasets to datasets for download

--- pretrain_model/download_datasets.sh
+++ pretrain_model/download_datasets.sh
@@ -1,4 +1,6 @@
 gsutil -m cp gs://search-query-classification-europe-west4-a/pretrain_datasets.zip .
 gsutil -m cp gs://search-query-classification-europe-west4-a/pretrain_tokeniser.zip .
+gsutil -m cp gs://search-query-classification-europe-west4-a/pretrain_text_datasets.zip .
 unzip pretrain_dataset.zip
 unzip pretrain_tokeniser.zip
+unzip pretrain_text_datasets.zip
