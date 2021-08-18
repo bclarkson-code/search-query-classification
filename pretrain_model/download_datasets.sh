5fd0abfee3540a87c10859c3156e626e9f2021fc
Benedict Clarkson <benedictc@spotify.com>
2021-08-18 13:09:13 +0100

    corrected spelling

--- pretrain_model/download_datasets.sh
+++ pretrain_model/download_datasets.sh
@@ -1,4 +1,4 @@
-gsutil -m cp gs://search-query-classification-europe-west4-a/pretrain_dataset.zip .
+gsutil -m cp gs://search-query-classification-europe-west4-a/pretrain_datasets.zip .
 gsutil -m cp gs://search-query-classification-europe-west4-a/pretrain_tokeniser.zip .
 unzip pretrain_dataset.zip
-unzip pretrain_tokniser.zip
+unzip pretrain_tokeniser.zip
