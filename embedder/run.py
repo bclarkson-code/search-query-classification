95ec48ae9d88803781f2ffd65233e2897bbea70f
Benedict Clarkson <benedictc@spotify.com>
2021-08-19 04:36:55 +0100

    using all 8 cores

--- embedder/run.py
+++ embedder/run.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
         mode='min',
     )
     trainer = pl.Trainer(
-        tpu_cores=1,
+        tpu_cores=8,
         max_epochs=3,
         progress_bar_refresh_rate=1,
         logger=tb_logger,
